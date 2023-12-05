dd4fcf71c9bb91302fe0453ac9a5a1f7e9e546e9
ehdtjs0612 <inko51366@naver.com>
2023-12-05 18:34:28 +0900

    resolve, reject 둘다 테스트

--- test/async.spec.ts
+++ test/async.spec.ts
@@ -19,4 +19,14 @@ describe(('Async 테스트'), () => {
 
 		expect(product).toEqual({ item: 'milk', price: 200 });
 	});
+
+	it('async-resolve를 테스트한다', () => {
+		return expect(fetchProduct()).resolves.toEqual({
+			item: 'milk', price: 200
+		});
+	});
+
+	it('async-reject를 테스트한다', () => {
+		return expect(fetchProduct('error')).rejects.toBe('에러요');
+	});
 });
